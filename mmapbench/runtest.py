07852758788d859c0e58511e6a6b9aa4c8023233
Yuanxuan Wang <zellux@gmail.com>
2010-01-09 15:54:45 +0000

    Add a test script for mmapbench

---
+++ mmapbench/runtest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import os, re
+from subprocess import *
+
+corelist = [1, 2, 3, 4]
+repeat = 3
+
+def warmup():
+	print('Warming up...')
+	for i in xrange(3):
+		p = Popen('./mmapbench 1', shell=True, stdout=PIPE)
+		os.waitpid(p.pid, 0)
+
+def test():
+	print('Begin testing...')
+	pattern = re.compile(r'usec: (\d+)')
+	for n in corelist:
+		total = 0
+		for i in xrange(repeat):
+			p = Popen('./mmapbench %d' % n, shell=True, stdout=PIPE)
+			os.waitpid(p.pid, 0)
+			output = p.stdout.read().strip()
+			usec = int(pattern.search(output).group(1))
+			total += usec
+		print('Core #%d: %d' % (n, total / repeat))
+
+warmup()
+test()
